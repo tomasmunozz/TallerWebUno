1a1a032c79de612e0be075c402b997a920ebe8d5
tomasmunozz <tomiroyale9@outlook.com>
2023-06-29 07:41:20 -0500

    taller 1 js

---
+++ script.js
@@ -0,0 +1,17 @@
+function cambiarEstilos() {
+    const conteiner = document.querySelector('#conteiner');
+    const form = document.querySelector('#form');
+    const icon = document.querySelector('#icon');
+    const Circle = document.querySelector('#Circle');
+    const Circle2 = document.querySelector('#Circle2');
+    const button = document.querySelector('#button');
+    const textp = document.querySelector('#texto');
+
+    conteiner.setAttribute('class', 'conteiner');
+    form.setAttribute('class', 'form');
+    icon.setAttribute('class', 'icon');
+    Circle.setAttribute('class', 'Circle');
+    Circle2.setAttribute('class', 'Circle2');
+    button.setAttribute('class', 'button');
+    texto.setAttribute('class', 'texto');
+}
